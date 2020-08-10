0674e084d0ed6843502d52dd826d0a13b2c25f88
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-10 16:54:28 -0700

    ready to run production migrations

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
+// module.exports = {
+//   PORT: process.env.PORT || 8000,
+//   NODE_ENV: process.env.NODE_ENV || 'development',
+//   DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:new123@localhost/poll_data',
+//   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+// };
+
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:new123@localhost/poll_data',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:new123@localhost/poll_data',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/poll_data',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
