0787a788baa5e17921f202f6562c9035157ab443
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-11 16:06:53 -0700

    config changes

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:new123@localhost/poll_data',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/poll_data',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  API_KEY: process.env.API_KEY || null,
+  DATABASE_URL: process.env.DB_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || '106816c9-bcb5-4a05-9d51-4dda16a357c3',
 };
