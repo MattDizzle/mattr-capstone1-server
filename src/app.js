c9ab2dc6368c6887122999ec9f8b3d194d71b9a9
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-27 22:38:37 -0700

    spring cleaning

--- src/app.js
+++ src/app.js
@@ -26,17 +26,15 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/candidate", candidateRouter);
 
-app.use(function errorHandler(error, req, res) {
+app.use(function errorHandler(error, req, res, next) {
   let response;
-
-  
-  if (NODE_ENV === "production") {
-    response = { error: "Server error" };
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
   } else {
-    console.error(error);
-    response = { error: error.message, object: error };
+    console.error(error)
+    response = { message: error.message, error }
   }
-  res.status(500).json(response);
-});
+  res.status(500).json(response)
+})
 
 module.exports = app;
